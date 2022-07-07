2c79b36927cda2971fd820c99110d8b2714061f0
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-07-07 15:06:47 +0200

    Add Profil Recruiter

---
+++ src/Controller/RecruiterController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recruiter;
+use App\Form\RecruiterType;
+use App\Repository\RecruiterRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/recruteur')]
+class RecruiterController extends AbstractController
+{
+    #[Route('/', name: 'app_recruiter_index', methods: ['GET'])]
+    public function index(RecruiterRepository $recruiterRepository): Response
+    {
+        return $this->render('pages/recruiter/index.html.twig', [
+            'recruiters' => $recruiterRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/creation', name: 'app_recruiter_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, RecruiterRepository $recruiterRepository): Response
+    {
+        $recruiter = new Recruiter();
+        $form = $this->createForm(RecruiterType::class, $recruiter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recruiterRepository->add($recruiter, true);
+
+            return $this->redirectToRoute('app_recruiter_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('pages/recruiter/new.html.twig', [
+            'recruiter' => $recruiter,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_recruiter_show', methods: ['GET'])]
+    public function show(Recruiter $recruiter): Response
+    {
+        return $this->render('recruiter/show.html.twig', [
+            'recruiter' => $recruiter,
+        ]);
+    }
+
+    #[Route('/{id}/edition', name: 'app_recruiter_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Recruiter $recruiter, RecruiterRepository $recruiterRepository): Response
+    {
+        $form = $this->createForm(RecruiterType::class, $recruiter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recruiterRepository->add($recruiter, true);
+
+            return $this->redirectToRoute('app_recruiter_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('pages/recruiter/edit.html.twig', [
+            'recruiter' => $recruiter,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_recruiter_delete', methods: ['POST'])]
+    public function delete(Request $request, Recruiter $recruiter, RecruiterRepository $recruiterRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$recruiter->getId(), $request->request->get('_token'))) {
+            $recruiterRepository->remove($recruiter, true);
+        }
+
+        return $this->redirectToRoute('app_recruiter_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/RecruiterType.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Form;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Entity\Recruiter;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class RecruiterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastname',TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Nom '
+
+            ])
+            ->add('firstname',TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Prénom'
+
+            ])
+             ->add('firmName',TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Nom de votre société'
+
+            ])
+            ->add('addressFirm',TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Adresse de la société'
+
+            ])
+           
+            ->add('zipcode',TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Code postal'
+
+            ])
+            ->add('city',TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Ville'
+
+            ])
+            //->add('isValided')
+            ->add('isRGPD',CheckboxType::class, [
+                'mapped' => true,
+                'label' => 'Je suis d\'accord avec le RGPD du site'
+            ])
+            ->add('user',TextType::class, [
+                'attr' => [
+                    'class' => 'form-control '
+                ],
+                'label' => 'Merci de confirmer votre adresse mail. '
+
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recruiter::class,
+        ]);
+    }
+}
